b9329b6b7f6ede87f567e5eda9ed8ec9912ca2e4
jdai-explore <jdaiexplore@gmail.com>
2025-07-11 20:46:30 +0200

    cleanup

--- arxml_viewer_pro/src/arxml_viewer/gui/controllers/__init__.py
+++
@@ -1,8 +0,0 @@
-# src/arxml_viewer/gui/controllers/__init__.py
-"""
-GUI Controllers - Navigation and interaction controllers
-"""
-
-from .navigation_controller import NavigationController
-
-__all__ = ['NavigationController']
